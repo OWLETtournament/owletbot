a9b795d6642d285b209231973eadcb1136c1653b
RainbowLegend <rainbowlegend3054@gmail.com>
2018-10-02 21:26:46 -0400

    typo

--- cogs/trades.py
+++ cogs/trades.py
@@ -11,8 +11,8 @@ class TradingSystem:
     @commands.command(name='diamondGraduate')
     async def diamond_graduate(self, ctx, name: str):
         servericon = ctx.guild.icon_url
-        diamond_em = discord.Embed(colour=0x8ce9ff, description="Congrats to " + discordTag.content +
-                                                                " on achieving Diamond! <:diamond:474220321562558464> ")
+        diamond_em = discord.Embed(colour=0x8ce9ff, description=f"Congrats to {name} on achieving Diamond! "
+                                                                f"<:diamond:474220321562558464> ")
         diamond_em.set_author(name="OWLET Player Graduate!", icon_url=servericon)
         channel = ctx.guild.get_channel(472450201009782787)
         await channel.send(embed=diamond_em)
