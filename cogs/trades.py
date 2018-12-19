3c6104ba85421fc97c39c4e437a0642ea9441ec5
danielnguyen <rainbowlegend3054@gmail.com>
2018-12-19 16:28:09 -0500

    Added trades

--- cogs/trades.py
+++ cogs/trades.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
 
-from discord.ext import commands
 import discord
+from discord.ext import commands
+
+from cogs.utils.enums import TradeEmotes
 
 
 class TradingSystem:
@@ -10,14 +12,75 @@ class TradingSystem:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='diamondGraduate')
-    async def diamond_graduate(self, ctx, name: str):
+    @commands.command(name='minorsgraduate',
+                      aliases=['diamond', 'diamondgraduate', 'minorsgrad', 'diamondgrad', 'minors'])
+    async def diamond_graduate(self, ctx, name):
         servericon = ctx.guild.icon_url
-        diamond_em = discord.Embed(colour=0x8ce9ff, description=f"Congrats to {name} on achieving Diamond! "
-                                                                f"<:diamond:474220321562558464> ")
-        diamond_em.set_author(name="OWLET Player Graduate!", icon_url=servericon)
-        channel = ctx.guild.get_channel(472450201009782787)
+        diamond_em = discord.Embed(colour=discord.Colour.teal(),
+                                   description=f"Congrats to {name} on achieving Diamond! <:diamond:474220321562558464>")
+        diamond_em.set_author(name="Minors Player Graduate!", icon_url=servericon)
+        channel = ctx.guild.get_channel(511076448388251669)
         await channel.send(embed=diamond_em)
+
+    @commands.command(name='majorsgraduate',
+                      aliases=['diamond', 'diamondgraduate', 'minorsgrad', 'diamondgrad', 'minors'])
+    async def diamond_graduate(self, ctx, name):
+        servericon = ctx.guild.icon_url
+        masters_em = discord.Embed(colour=discord.Colour.dark_gold(),
+                                   description=f"Congrats to {name} on achieving Masters! <:masters:525060384504414208>")
+        masters_em.set_author(name="Majors Player Graduate!", icon_url=servericon)
+        channel = ctx.guild.get_channel(511076448388251669)
+        await channel.send(embed=masters_em)
+
+    @commands.command(name='release', aliases=['drop'])
+    async def release(self, ctx, btag, team):
+        """Display that a player is being released by x team.
+
+        :param btag: Battle-tag of the player
+        :param team: Team being released from
+        """
+
+        rem = discord.Embed(colour=discord.Colour.red())
+        rem.description = f"{TradeEmotes.bnet} {btag}\n" \
+            f"{TradeEmotes.declined} {team}"
+        rem.set_author(name="OWLET Player Drop", icon_url=ctx.guild.icon_url)
+
+        channel = ctx.guild.get_channel(511076448388251669)
+        await channel.send(embed=rem)
+
+    @commands.command(name='pickup', aliases=['sign'])
+    async def pickup(self, ctx, btag, team):
+        """Display that a player is being signed by x team.
+
+        :param btag: Battle-tag of the player
+        :param team: Team being signed to
+        """
+
+        rem = discord.Embed(colour=discord.Colour.green())
+        rem.description = f"{TradeEmotes.bnet} {btag}\n" \
+            f"{TradeEmotes.accepted} {team}"
+        rem.set_author(name="OWLET Player Pickup", icon_url=ctx.guild.icon_url)
+
+        channel = ctx.guild.get_channel(511076448388251669)
+        await channel.send(embed=rem)
+
+    @commands.command(name='trade', aliases=['transfer'])
+    async def trade(self, ctx, btag, origin, destination):
+        """Display that a player is being traded by x team to y team.
+
+        :param btag: Battle-tag of the player
+        :param origin: Team being released from
+        :param destination: Team being traded to
+        """
+
+        rem = discord.Embed(colour=discord.Colour.gold())
+        rem.description = f"{TradeEmotes.bnet} {btag}\n" \
+            f"{TradeEmotes.declined} {origin}\n" \
+            f"{TradeEmotes.accepted} {destination}"
+        rem.set_author(name="OWLET Player Transfer", icon_url=ctx.guild.icon_url)
+
+        channel = ctx.guild.get_channel(511076448388251669)
+        await channel.send(embed=rem)
 
 
 def setup(bot):
---
+++ cogs/utils/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class TradeEmotes(Enum):
+    bnet = "<:battlenet:472564730079346699>"
+    accepted = "<:intick:472572212247920640>"
+    declined = "<:outtick:472572219734884362>"
