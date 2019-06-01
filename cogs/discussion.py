9dabeb6292aa83e1258accf9b1a766900a515477
RainbowLegend <rainbowlegend3054@gmail.com>
2019-06-01 12:39:15 -0400

    Update discussion.py

--- cogs/discussion.py
+++ cogs/discussion.py
@@ -17,9 +17,9 @@ class ModmailChecker:
         Takes in 2 parameters: a discussion name (MUST BE NO SPACES!!!!), and a discussion description.
         """
         discussion_cat = ctx.guild.get_channel(465948052025507860)
-        await ctx.guild.create_channel(discussion_name, category=discussion_cat, topic=discussion_description)
+        c = await ctx.guild.create_text_channel(discussion_name, category=discussion_cat, topic=discussion_description)
+        await ctx.send(f"Channel {c.mention} created successfully!")
             
-
 
 def setup(bot):
     bot.add_cog(ModmailChecker(bot))
