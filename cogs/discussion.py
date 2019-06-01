f4927374c67b56d4284f08b2eefcfef2cf50ff37
RainbowLegend <rainbowlegend3054@gmail.com>
2019-06-01 12:36:22 -0400

    Update discussion.py

--- cogs/discussion.py
+++ cogs/discussion.py
@@ -11,14 +11,14 @@ class ModmailChecker:
         self.bot.loop.create_task(self.__remind_checker())
 
     @commands.command(name="creatediscussion")
-        async def create_discussion(self, ctx, discussion_name, *, discussion_description):
-            """
-            Creates a discussion.
-            
-            Takes in 2 parameters: a discussion name (MUST BE NO SPACES!!!!), and a discussion description.
-            """
-            discussion_cat = ctx.guild.get_channel(465948052025507860)
-            await ctx.guild.create_channel(discussion_name, category=discussion_cat, topic=discussion_description)
+    async def create_discussion(self, ctx, discussion_name, *, discussion_description):
+        """
+        Creates a discussion.
+    
+        Takes in 2 parameters: a discussion name (MUST BE NO SPACES!!!!), and a discussion description.
+        """
+        discussion_cat = ctx.guild.get_channel(465948052025507860)
+        await ctx.guild.create_channel(discussion_name, category=discussion_cat, topic=discussion_description)
             
 
 
